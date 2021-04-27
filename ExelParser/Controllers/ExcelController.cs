052c0f3540d7f5c8cdd1b30c00903944ca97ac2b
EkhlasMridha <41969194+EkhlasMridha@users.noreply.github.com>
2021-04-28 00:51:29 +0600

    Excel to json parser

---
+++ ExelParser/Controllers/ExcelController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ExelParser.Services;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ExelParser.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExcelController : ControllerBase
+    {
+        ExcelService _excelService;
+
+        public ExcelController(ExcelService excelService)
+        {
+            _excelService = excelService;
+        }
+
+        // GET api/<ExcelController>/5
+        [HttpGet("{name}")]
+        public IActionResult Get(string name)
+        {
+            var dataTable = _excelService.ReadExcel(name);
+
+            var jsonDurty = JsonConvert.SerializeObject(dataTable, Formatting.Indented);
+
+            return Content(jsonDurty.ToString(), contentType:"application/json");
+        }
+    }
+}
---
+++ ExelParser/Services/ExcelService.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using DocumentFormat.OpenXml;
+using System.Data;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
+using System.Web;
+
+namespace ExelParser.Services
+{
+    public class ExcelService
+    {
+        public ExcelService()
+        {
+        }
+
+        public DataTable ReadExcel(string sheetName)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "ExcelSheets", "Paint_Calculator-1.xlsx");
+            DataTable dataTable = new DataTable();
+
+            SpreadsheetDocument doc = OpenSpreaSheet(path);
+
+            var workSheet = GetWorkSheetByName(sheetName, doc);
+
+            DataTable data = GetWorksheetToDataTable(workSheet,doc);
+
+            return data;
+        }
+
+        public Worksheet GetWorkSheetByName(string sheetname, SpreadsheetDocument doc)
+        {
+            WorkbookPart workbookPart = doc.WorkbookPart;
+            string relationId = workbookPart.Workbook.Descendants<Sheet>().FirstOrDefault(s => s.Name.Equals(sheetname))?.Id;
+
+            Worksheet worksheet = ((WorksheetPart)workbookPart.GetPartById(relationId)).Worksheet;
+
+            return worksheet;
+        }
+
+        public static SpreadsheetDocument OpenSpreaSheet(string path)
+        {
+            SpreadsheetDocument doc = SpreadsheetDocument.Open(path, false);
+
+            return doc;
+        }
+
+        public DataTable GetWorksheetToDataTable(Worksheet worksheet, SpreadsheetDocument doc)
+        {
+            WorkbookPart workbookPart = doc.WorkbookPart;
+            SheetData sheetData = worksheet.GetFirstChild<SheetData>();
+            DataTable dataTable = new DataTable();
+            int numOfColumn = sheetData.ElementAt(0).ChildElements.Count();
+
+            for(int rcnt = 0; rcnt < sheetData.ChildElements.Count; ++rcnt)
+            {
+                List<string> rowList = new List<string>();
+                var cnt = sheetData.ElementAt(rcnt).ChildElements.Count();
+                for (int rcnt2 = 0; rcnt2 < numOfColumn; ++rcnt2)
+                {
+                    Cell currentCell = (Cell)sheetData.ElementAt(rcnt).ChildElements.ElementAt(rcnt2);
+
+                    if(currentCell.DataType != null)
+                    {
+                        if(currentCell.DataType == CellValues.SharedString)
+                        {
+                            int id;
+                            if(Int32.TryParse(currentCell.InnerText, out id))
+                            {
+                                SharedStringItem sharedStringItem = workbookPart.SharedStringTablePart.
+                                                                    SharedStringTable.Elements<SharedStringItem>()
+                                                                    .ElementAt(id);
+                                if(sharedStringItem.Text != null)
+                                {
+                                    if(rcnt == 0)
+                                    {
+                                        dataTable.Columns.Add(sharedStringItem.Text.Text);
+                                    }
+                                    else
+                                    {
+                                        rowList.Add(sharedStringItem.Text.Text);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if(rcnt != 0)
+                        {
+                            rowList.Add(currentCell.InnerText);
+                        }
+                    }
+                }
+                if(rcnt != 0)
+                {
+                    dataTable.Rows.Add(rowList.ToArray());
+                }
+            }
+
+            return dataTable;
+        }
+           
+    }
+}
